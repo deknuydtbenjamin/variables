f1629f6ab3a932b87aafa35159946b404171b4d8
deknuydt benjamin <deknuydtbenjamin@hotmail.fr>
2024-10-07 10:59:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let variableOne = 1;
 let variableTwo = 2;
-let variableThree = 3;
+let variableThree = 3; 
 
-if(variableOne === variableTwo && variableTwo === variableThree){
+	if(variableOne === variableTwo && variableTwo === variableThree && variableOne === variableThree){
 	console.log("The three variables are the same");
 }
-if(variableOne === variableTwo || variableOne === variableThree || variableTwo ===variableThree){
+	else if(variableOne === variableTwo || variableOne === variableThree || variableTwo ===variableThree){
 	console.log("Two of the variables are the same");
 }
-if(variableOne !== variableTwo && variableTwo !== variableThree){
+	else{
 	console.log("all variables are different");
 }
